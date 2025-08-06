cb09cf26e414d155598fde44cec1a44eef037e8d
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2025-08-06 16:23:26 +0700

    keycloak project init

---
+++ pkce/src/main/java/com/keycloak/pkce/PkceApplication.java
@@ -0,0 +1,13 @@
+package com.keycloak.pkce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkceApplication.class, args);
+	}
+
+}
---
+++ pkce/src/test/java/com/keycloak/pkce/PkceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keycloak.pkce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
